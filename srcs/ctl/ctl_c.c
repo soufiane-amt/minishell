1414cafddb4747a773023496f20e489f756d02ec
El mehdi Elmohammady <eelmoham@e1r4p12.1337.ma>
2022-05-14 15:58:35 +0100

    deleted:    E         deleted:    EO         deleted:    EOF         deleted:    lol         deleted:    minishell         deleted:    note.txt         deleted:    out         deleted:    outfile         deleted:    tests/.temp         deleted:    tests/a.out.dSYM/Contents/Info.plist         deleted:    tests/a.out.dSYM/Contents/Resources/DWARF/a.out         deleted:    tests/exec_cmd_test.c         deleted:    tests/file.txt         deleted:    tests/global_v.c         deleted:    tests/minishell         deleted:    tests/outfile         deleted:    tests/parse_arg_to_var         deleted:    tests/parse_arg_to_var.c         deleted:    tests/path.c         deleted:    tests/pipe_test.c         deleted:    tests/remove_doubleQt         deleted:    tests/remove_doubleQt.c         deleted:    tests/signals.c         deleted:    tests/te.c         deleted:    tests/test.c         deleted:    tests/test_ctl_c.c         deleted:    tests/test_extract.c         deleted:    tests/test_lens.c         deleted:    tests/test_readln.c         deleted:    tests/test_split.c         deleted:    tests/test_syntax.c         deleted:    tests/test_void         deleted:    tests/test_void.c         deleted:    tests/writing.c         deleted:    text

--- srcs/ctl/ctl_c.c
+++ srcs/ctl/ctl_c.c
@@ -3,50 +3,50 @@
 /*                                                        :::      ::::::::   */
 /*   ctl_c.c                                            :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: samajat <samajat@student.42.fr>            +#+  +:+       +#+        */
+/*   By: eelmoham <eelmoham@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/03/28 12:15:27 by eelmoham          #+#    #+#             */
-/*   Updated: 2022/04/21 15:53:06 by samajat          ###   ########.fr       */
+/*   Updated: 2022/05/14 15:57:29 by eelmoham         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minishell.h"
 
-// void ctl_c(int sig)
-// {
-//     struct termios    nt;
-//     if (sig == SIGINT)
-//     {
-//         printf("\r");
-//         rl_on_new_line();
-//         rl_redisplay();
-//         printf("\n");
-//         rl_on_new_line();
-//         rl_replace_line("", 0);
-//         rl_redisplay();
-//         nt.c_lflag &= ~(ECHOCTL);
-//         *(data.status.exit_code) = 128 + sig;
-//     }
-//     if (sig == SIGQUIT)
-//     {
-//         *(data.status.exit_code) = 128 + sig;
-//         exit(1);
-//     }//we should use exit function for free 
-//     nt.c_lflag |= ECHOCTL;
-// }
-
 void ctl_c(int sig)
 {
+    struct termios    nt;
     if (sig == SIGINT)
     {
-	    printf("\r");
-		rl_on_new_line();
-		rl_redisplay();
+        printf("\r");
+        rl_on_new_line();
+        rl_redisplay();
         printf("\n");
-		rl_on_new_line();
-		rl_replace_line("", 0);
-		rl_redisplay();
+        rl_on_new_line();
+        rl_replace_line("", 0);
+        rl_redisplay();
+        nt.c_lflag &= ~(ECHOCTL);
+        *(data.status.exit_code) = 128 + sig;
     }
     if (sig == SIGQUIT)
+    {
+        *(data.status.exit_code) = 128 + sig;
         exit(1);
+    }//we should use exit function for free 
+    nt.c_lflag |= ECHOCTL;
 }
+
+// void ctl_c(int sig)
+// {
+//     if (sig == SIGINT)
+//     {
+// 	    printf("\r");
+// 		rl_on_new_line();
+// 		rl_redisplay();
+//         printf("\n");
+// 		rl_on_new_line();
+// 		rl_replace_line("", 0);
+// 		rl_redisplay();
+//     }
+//     if (sig == SIGQUIT)
+//         exit(1);
+// }
