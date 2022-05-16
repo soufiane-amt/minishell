44fcbfcfc6d63261a983961ff5ffb46ecef5cc73
Soufiane Amajat <samajat@e2r4p1.1337.ma>
2022-05-16 23:30:21 +0100

    siyebt ddak lmochkil dyal double _char

--- srcs/parsing/ft_extractor.c
+++ srcs/parsing/ft_extractor.c
@@ -6,7 +6,7 @@
 /*   By: samajat <samajat@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/04/12 02:13:21 by samajat           #+#    #+#             */
-/*   Updated: 2022/05/16 22:03:38 by samajat          ###   ########.fr       */
+/*   Updated: 2022/05/16 23:23:44 by samajat          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -130,19 +130,18 @@ void    ft_extract_data(t_cmd *cmd, char    *command)
     
     int         a;
 
-    if (!(command + data.e) && !command[data.e])
-        return ;
     if (extract_norm(cmd, command, &data.e))
         a = 0;
     else if (extract_redir(cmd, command, &data.e))
         a = 0;
     else if (extract_quote(cmd, command, &data.e))
         a = 0;
-    if (command[data.e])
+    if ((command + data.e) && command[data.e])
     {
         ft_extract_data(cmd, command);
         if (*data.status.exit_code)
             return ;
     }
-    ft_replace_with_acctual_values(cmd);
+    else
+        ft_replace_with_acctual_values(cmd);
 }
